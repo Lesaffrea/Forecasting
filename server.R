9e0007119fc056c8154117d11be022e1d143b3a3
Lesaffrea <alainlesaffre@hotmail.com>
2015-03-29 11:30:10 +0800

    Add reactive code for the production data set

--- server.R
+++ server.R
@@ -11,7 +11,7 @@ library(lubridate)
 library(BH)
 options(RCHART_WIDTH = 800)
 
-# We put it there to see 
+# Load the three years of production 
 GetDataRWE <-function(){
         filename =c("data/E_round3_train.csv")
         train.columns <-c('character',    # Date
@@ -29,6 +29,25 @@ GetDataRWE <-function(){
         initialtrainingset
 }
 
+# Load the outliers 
+
+GetOutliers <-function(){
+        filename =c("data/outliers.csv")
+        train.columns <-c('character',    # Date
+                          'numeric',      # Temp
+                          'numeric',      # Wind_Speed
+                          'numeric',      # Precip_Amt
+                          'numeric',      # Solar_Rad
+                          'factor',       # Day_Type
+                          'factor',       # School_Holyday
+                          'factor',       # Winter
+                          'numeric'       # Volume
+        )
+        outliersset <-read.csv(filename, colClasses= train.columns, header=TRUE)
+        outliersset$Date <-gsub(c("/"), c("-"), outliersset$Date)
+        outliersset
+}
+
 #   ---------------------
 #   Build the fix objects
 #   ---------------------
@@ -37,6 +56,7 @@ trainingset$Date <-sub(c(" UTC"),"", dmy(trainingset$Date))
 trainingset12 <-trainingset[year(trainingset$Date) %in% c("2012"),]
 trainingset13 <-trainingset[year(trainingset$Date) %in% c("2013"),]
 trainingset14 <-trainingset[year(trainingset$Date) %in% c("2014"),]
+
 #
 # We build the graphic data for the trend analysis
 #
@@ -52,6 +72,13 @@ tsprod <-ts(trainingset$volume, start=c(year(trainingset$Date[1]),
 #
 trendseason <-stl(tsprod, "periodic")
 rm(trainingset)
+#--------------------------
+#      Outliers LOad 
+#--------------------------
+exceptions <-GetOutliers()
+exceptions <-exceptions[,c("Date", "volume")]
+
+
 
 
 
@@ -60,15 +87,15 @@ rm(trainingset)
 shinyServer(function(input, output) {
         
 output$distPlot <- renderChart({
-        
-        selected <-paste0(c("y"), input$dispyear)
-        todisplay<- switch(selected,
+        todisplay <-reactive({
+                  selected <-paste0(c("y"), input$dispyear)
+                  switch(selected,
                         y2012 = trainingset12,
                         y2013 = trainingset13,
                         y2014 = trainingset14 )
+        })
          
-         
-         Rmgraph <-mPlot(x = "Date", y = "volume", type = "Line", data = todisplay,
+         Rmgraph <-mPlot(x = "Date", y = "volume", type = "Line", data = todisplay(),
                           pointSize = 0, lineWidth = 1 )
          Rmgraph$set(dom = 'distPlot')
          return(Rmgraph)
@@ -77,6 +104,10 @@ output$distPlot <- renderChart({
  output$timeserieplot <-renderPlot({
            threeyears<-plot(trendseason, main="Three production overview with STL decomposition")
            return(threeyears)
+          })
+ 
+ output$exceptions <-renderTable({
+          head(exceptions, n= 20)                 
           })
     
     
